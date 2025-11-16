efae49fb93b19653dd7d0de8b4de0dbd4dee5f39
Abhishek AR <122305073+abhishekpythoninmakes@users.noreply.github.com>
2025-11-16 14:39:01 +0530

    new update in settings for static files

--- csc_academy/settings.py
+++ csc_academy/settings.py
@@ -95,20 +95,18 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",   
+]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Folder where Django will collect all static files
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Extra static directories for development
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
-# Media files
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 
 # Default primary key field type
