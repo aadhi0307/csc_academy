291449365473c02d6f77e8f98b569b60bd651864
ADARSH SASI <101856495+aadhi0307@users.noreply.github.com>
2025-11-17 15:50:00 +0530

    updated code

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from myapp import views
 
 urlpatterns = [
-    path('index/', views.index, name='index'),
+    path('', views.index, name='index'), 
     path('about/', views.about, name='about'),
     path('affiliations/', views.affiliations, name='affiliations'),
     path('contact/', views.contact, name='contact'),
     path('contact_data/', views.contact_data, name='contact_data'),
     path('course/', views.course, name='course'),
-    path('course/<int:course_id>/', views.course_detail, name='course_detail'),  # new
+    path('course/<int:course_id>/', views.course_detail, name='course_detail'),
 ]
